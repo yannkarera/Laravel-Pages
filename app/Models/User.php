f840dc5fa9731ed34ced524256169aeb70829281
Yann Karera <y_karera@outlook.be>
2026-01-28 20:06:02 +0100

    mise en place du model pour enregistrement dans DB

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
     ];
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+{
+    return $this->hasMany(Post::class);
 }
+
+}
+    
